af16010cb78e7060adb9d56ab52ba8d57db18b32
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2023-02-09 22:33:53 -0500

    Almost Player Running Animation
    Almost done with player running animation

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ pygame.display.set_icon(game_icon)
 black = (0, 0, 0)
 white = (255, 255, 255)
 green = (0, 255, 5)
+red = (255, 0, 0)
 #player
-player_sprite = pygame.image.load("player_sprite.png")
-player_sprite = pygame.transform.scale(player_sprite, (100, 100))
 player_color = (3, 25, 255)
-#enemy
-enemy_ai = True
 #score and font
 font = pygame.font.SysFont("comicsansms", 35)
 #clock
@@ -44,7 +41,7 @@ map1 = """
 
 
 
-
+g                         g
 
 
 
@@ -70,8 +67,8 @@ ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"""
 
 map1 = map1.splitlines()
 
-for line in map1:
-    print(line)
+#for line in map1:
+ #   print(line)
 
 grass_tile = pygame.image.load("grass_tile.png")
 grass_tile = pygame.transform.scale(grass_tile, (75, 75))
@@ -95,40 +92,40 @@ def quit_game():
 def game_loop():
     #gravity
     player_y_gravity = 1
-    player_jump_height = 20
+    player_jump_height = 22.5
     player_y_velocity = player_jump_height
     #player
+    player_sprite = pygame.image.load("player_sprite.png")
+    player_sprite = pygame.transform.scale(player_sprite, (125, 125))
+    player_moving = False
     player_jumping = False
     player_dead = False
     player_x = displaysurf_width/2
-    player_y = displaysurf_height/2
+    player_y = displaysurf_height/2.14
     player_loc = player_x, player_y
     player_speed = 5
     player_direction = "west"
     player_rect = displaysurf.blit(player_sprite, (player_x, player_y))
-    #enemy
-    enemy_move_choice_list = ["north", "south", "west", "east"]
-    enemy_random_move = random.choice(enemy_move_choice_list)
-    enemy_x = displaysurf_width/4
-    enemy_y = displaysurf_height/4
-    default_enemy_speed = 0.01
-    enemy_speed = default_enemy_speed
-    enemy_color = (255, 0, 15)
     #fps
     fps = Clock.get_fps()
     while not player_dead:
         fps_text = font.render(str(fps)+" FPS", True, green)
         displaysurf.fill(black)
-        enemy_speed += 0.0001
         player_loc = player_x, player_y
-        enemy_speed = default_enemy_speed
             
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
         keys = pygame.key.get_pressed()
-        player_x += (keys[pygame.K_d] - keys[pygame.K_a]) * player_speed
+        if keys[pygame.K_d]:
+            player_x += player_speed
+            player_direction = "east"
+            player_moving = True
+        if keys[pygame.K_a]:
+            player_x -= player_speed
+            player_direction = "west"
+            player_moving = True
         if keys[pygame.K_w]:
             player_jumping = True
         if not player_jumping:
@@ -140,13 +137,26 @@ def game_loop():
                 player_jumping = False
                 player_y_velocity = player_jump_height
             player_rect = displaysurf.blit(player_sprite, (player_x, player_y))
+        if not player_moving:
+            player_direction = "NOWHERE"
+        if player_direction == "west":
+            player_sprite = pygame.transform.flip(player_sprite, 0, 0)
+            player_rect = displaysurf.blit(player_sprite, (player_x, player_y))
+        if player_direction == "east":
+            player_sprite = pygame.transform.flip(player_sprite, 180, 0)
+            player_rect = displaysurf.blit(player_sprite, (player_x, player_y))
+        if player_moving:
+            player_sprites = []
+            player_sprites.append(pygame.image.load('player_running_1.png'))
+            player_sprites.append(pygame.image.load('player_running_2.png'))
+            player_sprites.append(pygame.image.load('player_running_3.png'))
+            player_rect = displaysurf.blit(player_sprite, (player_x, player_y))
         fps = Clock.get_fps()
         fps = round(fps)
         displaysurf.blit(fps_text, (displaysurf_width / 100, displaysurf_height / 100))
         tiles(map1)
         Clock.tick(60)
         pygame.display.update()
-
 
     displaysurf.fill(black)
     while player_dead:
